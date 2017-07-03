8102878f9409863f05050aaa0ee8e9e5339bd91d
Samuel Neugber <samuel.neugber@softwire.com>
2017-07-03 15:56:53 +0100

    Added sample battleship implementation

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var Battleships = require('./src/Battleships.js');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.get('/GetShipPositions', function(req, res) {
+    var positions = Battleships.getShipPositions();
+    res.send(positions);
+});
+
+app.post('/SelectTarget', function(req, res) {
+    var target = Battleships.selectTarget(req.body);
+    res.send(target);
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
---
+++ src/Battleships.js
@@ -0,0 +1,40 @@
+function getShipPositions() {
+    return [
+      { StartingSquare: { Row: "A", Column: 1 }, EndingSquare : { Row: "A", Column: 5 } },
+      { StartingSquare: { Row: "C", Column: 1 }, EndingSquare : { Row: "C", Column: 4 } },
+      { StartingSquare: { Row: "E", Column: 1 }, EndingSquare : { Row: "E", Column: 3 } },
+      { StartingSquare: { Row: "G", Column: 1 }, EndingSquare : { Row: "G", Column: 3 } },
+      { StartingSquare: { Row: "I", Column: 1 }, EndingSquare : { Row: "I", Column: 2 } },
+    ]
+}
+
+function getNextTarget(position) {
+  var column = getNextColumn(position.Column);
+  var row = column === 1 ? getNextRow(position.Row) : position.Row;
+  return { Row: row, Column: column }
+}
+
+function getNextRow(row) {
+  var newRow = row.charCodeAt(0) + 1;
+  if(newRow > 'J'.charCodeAt(0)) {
+    return 'A';
+  }
+  return String.fromCharCode(newRow);
+}
+
+function getNextColumn(column) {
+  return column % 10 + 1;
+}
+
+function selectTarget(gamestate) {
+  var previousShot = gamestate.MyShots && gamestate.MyShots[gamestate.MyShots.length-1];
+  if(previousShot) {
+    return getNextTarget(previousShot.Position);
+  }
+  return { Row: "A", Column: 1 };  
+}
+
+module.exports = { 
+  getShipPositions: getShipPositions,
+  selectTarget: selectTarget
+};
